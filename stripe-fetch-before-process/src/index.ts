9ee8693fea306f412a94b3af7aba2107d7ab8a1b
Phil Leggetter <phil@leggetter.co.uk>
2025-07-01 11:07:35 +0100

    chore: update to fetch resource rather than event

--- stripe-fetch-before-process/src/index.ts
+++ stripe-fetch-before-process/src/index.ts
@@ -5,38 +5,64 @@ import Stripe from "stripe";
 import { verifyHookdeck } from "./utils/webhooks";
 
 const app = express();
-app.use(
-  express.json({
-    // Store the rawBody buffer on the request
-    verify: (req, res, buf) => {
-      (req as any).rawBody = buf;
-    },
-  })
-);
+app.use(express.raw({ type: "application/json" }));
 const port = process.env.PORT || 3456;
 
-if (!process.env.STRIPE_API_KEY) {
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY;
+if (!STRIPE_API_KEY) {
   throw new Error("Missing STRIPE_API_KEY environment variable");
 }
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY);
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
+if (!STRIPE_WEBHOOK_SECRET) {
+  throw new Error("Missing STRIPE_WEBHOOK_SECRET environment variable");
+}
+
+const stripe = new Stripe(STRIPE_API_KEY);
 
 app.post(
   "/api/stripe/invoices",
   verifyHookdeck,
   async (req: Request, res: Response) => {
     try {
-      // Fetch the full event data from Stripe using the event ID
+      // Check Stripe signature and construct the Stripe event
+      const sig = req.headers["stripe-signature"] as string;
+      let event: Stripe.Event;
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          sig,
+          STRIPE_WEBHOOK_SECRET,
+          // Disable timestamp checking since event was already check by Hookdeck
+          // This also allows for failed events to be replayed.
+          -1
+        );
+      } catch (err) {
+        console.log(`❌ Error:`, err);
+        res.status(400).json(err);
+        return;
+      }
+
+      if (event.type.startsWith("invoice.") === false) {
+        res.status(400).send("Unexpected event type");
+        return;
+      }
+
+      // Initially the webhook contains a snapshot of the invoice
+      const invoiceSnapshot = event.data.object as Stripe.Invoice;
+      const invoiceId = invoiceSnapshot.id;
+      if (!invoiceId) {
+        res.status(400).send("Invoice ID is missing");
+        return;
+      }
+
+      // Retrieve the latest version of the invoice
       // You will not hit the Stripe API rate limit here
-      const eventId = req.body.id;
-      const event = await stripe.events.retrieve(eventId);
-      const type = event.type;
-      const resource = event.data.object;
+      const invoice = await stripe.invoices.retrieve(invoiceId);
+      console.log(`Processing event type: ${event.type}`);
+      console.log(`Invoice:`, invoice);
 
-      console.log(`Processing event type: ${type}`);
-      console.log(`Resource:`, resource);
-
-      // Now you can process the resource, knowing it's the latest version
+      // Now you can process the invoice, knowing it's the latest version
       // ...
 
       res.sendStatus(200);
--- stripe-fetch-before-process/src/utils/webhooks.ts
+++ stripe-fetch-before-process/src/utils/webhooks.ts
@@ -1,27 +1,27 @@
-import crypto from 'crypto';
-import { Request, Response, NextFunction } from 'express';
+import crypto from "crypto";
+import { Request, Response, NextFunction } from "express";
 
 if (!process.env.HOOKDECK_WEBHOOK_SECRET) {
   throw new Error("HOOKDECK_WEBHOOK_SECRET not set");
 }
 
-export const verifyHookdeck = (req: Request, res: Response, next: NextFunction) => {
-  //Extract x-hookdeck-signature and x-hookdeck-signature-2 headers from the request
+export const verifyHookdeck = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const hmacHeader = req.get("x-hookdeck-signature");
   const hmacHeader2 = req.get("x-hookdeck-signature-2");
 
-  //Create a hash based on the parsed body
   const hash = crypto
     .createHmac("sha256", process.env.HOOKDECK_WEBHOOK_SECRET as string)
-    .update((req as any).rawBody)
+    .update(req.body)
     .digest("base64");
 
-  // Compare the created hash with the value of the x-hookdeck-signature and x-hookdeck-signature-2 headers
   if (hash === hmacHeader || (hmacHeader2 && hash === hmacHeader2)) {
-    console.log("Webhook is originating from Hookdeck");
     next();
   } else {
-    console.log("Signature is invalid, rejected");
+    console.error("Signature is invalid, rejected");
     res.sendStatus(403);
   }
 };
